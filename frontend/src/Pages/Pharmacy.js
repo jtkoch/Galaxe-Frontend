f57afdc8dc92aea93ac997f999046e7991d56009
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2021-02-02 14:32:11 -0600

    Bringing google maps into Pharmacy.js

--- frontend/src/Pages/Pharmacy.js
+++ frontend/src/Pages/Pharmacy.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-function Pharmacy() {
+const Pharmacy = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+    static defaultProps = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
+    };
+ 
+  render() {
     return (
-        <div>
-            <h1>Pharmacy</h1>
-        </div>
-    )
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
 export default Pharmacy
